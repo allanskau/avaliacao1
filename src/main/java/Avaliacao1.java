3e0c7848b947da52f74388c5af6a5c147a918289
allan <allan@192.168.108.171>
2015-05-12 12:09:44 -0300

    primeiro commit

--- src/main/java/Avaliacao1.java
+++ src/main/java/Avaliacao1.java
@@ -1,3 +1,6 @@
+
+import utfpr.ct.dainf.if62c.avaliacao.Complexo;
+
 /**
  * UTFPR - Universidade Tecnológica Federal do Paraná
  * DAINF - Departamento Acadêmico de Informática
@@ -7,7 +10,17 @@
  * @author 
  */
 public class Avaliacao1 {
-
+    
+    public static Complexo[] raizesEquacao(Complexo a, Complexo b, Complexo c){
+        Complexo[] raizesEquacao = new Complexo[2];
+        //raizesEquacao = sqrt(b);
+        //raizesEquacao = sqrt(((b.prod(b)).sub(a.prod(c).prod(4))));
+        //raizesEquacao[0] = (b.prod(-1).soma((b.prod(b)).sub(a.prod(c).prod(4))));
+        raizesEquacao[0] = b.prod(-1).soma(raizesEquacao[0]).div(a.prod(2));
+        raizesEquacao[1] = b.prod(-1).soma(raizesEquacao[1]).div(a.prod(2));
+        
+        return raizesEquacao;
+    }
     public static void main(String[] args) {
         // implementar main
     }
--- src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
+++ src/main/java/utfpr/ct/dainf/if62c/avaliacao/Complexo.java
@@ -17,29 +17,76 @@ public class Complexo {
 
     public Complexo(double real, double img) {
         // completar a implementação
+        this.real = real;
+        this.img = img;
     }
+    
 
     // implementar getReal()
-
+    public double getReal(){
+        return real;
+    }
+    
     // implementar getImg()
+    public double getImg(){
+        return img;
+    }
 
     public Complexo soma(Complexo c) {
         return new Complexo(real + c.real, img + c.img);
     }
     
     // implementar sub(Complexo)
+    public Complexo sub(Complexo c){
+        return new Complexo(real - c.real, img - c.img);
+    }
 
     // implementar prod(double)
+    public Complexo prod(double multiplicador){
+        return new Complexo(real * multiplicador, img * multiplicador);
+    }
 
     // implementar prod(Complexo)
+    public Complexo prod(Complexo c){
+        return new Complexo((real * c.real) - (img * c.img), (real * c.img) + (img * c.real));
+    }
     
     // implementar div(Complexo)
+    public Complexo div(Complexo c){
+        return new Complexo(((c.real*real + c.img*img) / (real*real + img*img)), // parte real
+                            (real*c.img - c.real*img) / (real*real + img*img) ); // parte imaginaria
+    }
     
     // implementar sqrt()
-    public Complexo[] sqrt() {
+    public Complexo[] sqrt(Complexo c) {
         // completar implementação
+        Complexo[] raizes = new Complexo[2];
+        
+        double r = Math.sqrt((c.real*c.real) + (c.img*c.img));
+        double rho = Math.sqrt(r);
+        double psi1;
+        double psi2;
+        double gama=0;
+        if(c.real > 0){
+            gama = Math.atan(c.img / c.real);
+        }else if(c.real < 0){
+            gama = Math.atan((c.img / c.real) + Math.PI);
+        }else if((c.real == 0) && (c.img == 0)){
+            gama = 0;
+        }else if((c.real == 0) && (c.img > 0)){
+            gama = Math.PI / 2;
+        }else if((c.real == 0) && (c.img < 0)){
+            gama = (3 * Math.PI) / 2;
+        }
+        psi1 = gama / 2;
+        psi2 = (gama / 2) + Math.PI;
+        
+        raizes[0] = new Complexo(rho*(Math.acos(psi1)), rho*(Math.asin(psi1)));
+        raizes[1] = new Complexo(rho*(Math.acos(psi2)), rho*(Math.asin(psi2)));
+        
         // retornar o vetor contendo as raízes
-        return null;
+        return raizes;
+        //return null;
     }
 
     @Override
